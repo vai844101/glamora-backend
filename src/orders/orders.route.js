9894eedc4f5430326a892d6a36e8d7fdbc1eead1
Vaibhav Kumar <kumarvaibhav844101@gmail.com>
2025-01-29 23:17:35 +0530

    Update orders.route.js

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -25,8 +25,8 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:5173/cancel`,
+      success_url: `https://glamora-frontend-nu.vercel.app/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://glamora-frontend-nu.vercel.app/cancel`,
     });
 
     res.json({ id: session.id });
