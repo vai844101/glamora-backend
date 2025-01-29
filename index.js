1798baa310a6a3e6ccf5c3d02cd925c712c67671
Vaibhav Kumar <kumarvaibhav844101@gmail.com>
2025-01-29 23:16:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://glamora-frontend-nu.vercel.app",
     credentials: true,
   })
 );
